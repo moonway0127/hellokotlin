6d1b47b79c9123c187ce492e89a93067f243aa3c
moonway <moonway0127@gmail.com>
2017-11-14 16:35:57 +0800

    增加新内容

---
+++ IdeaProjects/hellokotlin/src/xin/moonway/_1_foundation/_10_while/while.kt
@@ -0,0 +1,16 @@
+package xin.moonway._1_foundation._10_while
+
+fun main(args: Array<String>) {
+    var i = 0
+    while (i++ < 10) {
+        println(i)
+    }
+
+    do {
+        if (i == 6)
+            continue
+        println(i)
+        if (i == 5)
+            break
+    } while (--i > 0)
+}
